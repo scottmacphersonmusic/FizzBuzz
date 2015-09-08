942bd93b0603f88745421e4816630e094b11cc20
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-08 14:29:53 -0700

    Obey Rubocop

--- lib/arbitrary.rb
+++ lib/arbitrary.rb
@@ -1,12 +1,12 @@
-def arb_fizz(limit, name_hash)
+def arb_fizz(limit, word_hash)
   numbers = (2..limit).to_a
-  keys = name_hash.keys
-  fizz = ["0", "1"]
+  keys = word_hash.keys
+  fizz = %w(0 1)
   numbers.each do |num|
     output = ""
     matches = keys.select { |key| num % key == 0 }
-    unless matches.empty?
-      matches.each { |match| output += name_hash[match] }
+    if matches.any?
+      matches.each { |match| output += word_hash[match] }
     else
       output += num.to_s
     end
--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,9 +2,7 @@ def fizzbuzz(first, last)
   num_array = (first..last).to_a
   num_array.each do |num|
     output = ""
-    if (num % 3 != 0) && (num % 5 != 0)
-      output += num.to_s
-    end
+    output += num.to_s if (num % 3 != 0) && (num % 5 != 0)
     output += "Fizz" if num % 3 == 0
     output += "Buzz" if num % 5 == 0
     puts output
